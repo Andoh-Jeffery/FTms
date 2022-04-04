85c75f3077ab1e5ed43c5e307a7ceb3961891137
TroyLynx <58670183+Andoh-Jeffery@users.noreply.github.com>
2022-04-04 19:54:22 +0100

    fasteck
    new repo

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const path=require('path');
+require('dotenv').config();
+
+const app=express();
+// middlewares
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'./styles')));
+app.use(express.static(path.join(__dirname,'./images')));
+
+// APIs
+
+app.get('/',(req,res)=>{
+    res.render('index',{title:'Hompage'});
+});
+
+app.listen(process.env.PORT,console.log(`server is running on port: ${process.env.PORT}`));
