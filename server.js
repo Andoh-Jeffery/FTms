4d9061d539e7a86a0d0ee4ed486fa34cfd1e1731
TroyLynx <ajongbahj@gmail.com>
2022-04-05 01:29:34 +0200

    made certain changes and added files

--- server.js
+++ server.js
@@ -15,5 +15,8 @@ app.use(express.static(path.join(__dirname,'./images')));
 app.get('/',(req,res)=>{
     res.render('index',{title:'Hompage'});
 });
+app.get('/login',(req,res)=>{
+    res.render('login',{title:'login'});
+})
 
 app.listen(process.env.PORT,console.log(`server is running on port: ${process.env.PORT}`));
